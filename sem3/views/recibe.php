ba73eb170127a281c8d9ec3c9ba8c236b4d45003
diego <90421880+diegoalonsonm@users.noreply.github.com>
2024-10-02 20:56:52 -0600

    sem 3

---
+++ sem3/views/recibe.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>semana 3 recibe</title>
+</head>
+<body>
+    <?php
+        $nombre = $_POST['nombre'];
+        echo "Hola $nombre";
+    ?>
+</body>
+</html>
